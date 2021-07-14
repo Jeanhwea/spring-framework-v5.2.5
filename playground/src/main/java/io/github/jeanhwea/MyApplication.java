af354b2ed163b2911abb00ed40ce01e0db533120
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-14 21:59:14 +0800

    add: 添加 AnnotationConfigApplicationContext

--- playground/src/main/java/io/github/jeanhwea/MyApplication.java
+++ playground/src/main/java/io/github/jeanhwea/MyApplication.java
@@ -1,15 +1,19 @@
 package io.github.jeanhwea;
 
 import io.github.jeanhwea.bean.Author;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApplication {
 
   public static void main(String[] args) {
-    ClassPathXmlApplicationContext ctx =
+    ClassPathXmlApplicationContext ctx1 =
         new ClassPathXmlApplicationContext("META-INF/my-beans.xml");
+    Author author1 = ctx1.getBean("author", Author.class);
+    System.out.println(author1.getName());
 
-    Author person = ctx.getBean("author", Author.class);
-    System.out.println(person.getName());
+    AnnotationConfigApplicationContext ctx2 = new AnnotationConfigApplicationContext(Author.class);
+    Author author2 = ctx2.getBean("author", Author.class);
+    System.out.println(author2.getName());
   }
 }
--- playground/src/main/java/io/github/jeanhwea/bean/Author.java
+++ playground/src/main/java/io/github/jeanhwea/bean/Author.java
@@ -1,6 +1,10 @@
 package io.github.jeanhwea.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Author {
+
+  @Value("Tom Wang")
   private String name;
 
   public String getName() {
