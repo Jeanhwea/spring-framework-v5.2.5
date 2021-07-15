9fa6bc24605a957b1b4077ea5fbad6a343c36866
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-15 16:51:42 +0800

    add: 添加 BeanLife 的测试代码

--- playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
+++ playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
@@ -1,13 +1,15 @@
 package io.github.jeanhwea.app02;
 
 import io.github.jeanhwea.app02.lifecycle.BeanLife;
+import io.github.jeanhwea.app02.lifecycle.BeanLifeBeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp02 {
 
   public static void main(String[] args) {
     System.out.println("==================== BEGIN ====================");
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeanLife.class);
+    AnnotationConfigApplicationContext ctx =
+        new AnnotationConfigApplicationContext(BeanLifeBeanPostProcessor.class, BeanLife.class);
     // BeanLife beanLife = ctx.getBean("beanLife", BeanLife.class);
     System.out.println("====================  END  ====================");
   }
---
+++ playground/src/main/java/io/github/jeanhwea/app02/lifecycle/BeanLifeBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package io.github.jeanhwea.app02.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanLifeBeanPostProcessor implements BeanPostProcessor {
+
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName)
+      throws BeansException {
+    if (bean instanceof BeanLife) {
+      System.out.println("postProcessBeforeInitialization");
+    }
+    return bean;
+  }
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    if (bean instanceof BeanLife) {
+      System.out.println("postProcessAfterInitialization");
+    }
+    return bean;
+  }
+}
