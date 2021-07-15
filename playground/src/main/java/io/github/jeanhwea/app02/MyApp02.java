98df24ac1abb8c1d7f49f121c9077d846d22b4a3
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-15 18:33:31 +0800

    add: 添加定义 Bean 的方式

--- playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
+++ playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
@@ -1,15 +1,22 @@
 package io.github.jeanhwea.app02;
 
+import io.github.jeanhwea.app02.lifecycle.BeanLife;
+import io.github.jeanhwea.app02.lifecycle.BeanLifeBeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp02 {
 
   public static void main(String[] args) {
     System.out.println("==================== BEGIN ====================");
-    AnnotationConfigApplicationContext ctx =
-        new AnnotationConfigApplicationContext("io.github.jeanhwea.app02");
-    // ctx.registerBean(BeanLifeBeanPostProcessor.class);
-    // ctx.registerBean(BeanLife.class);
+
+    // AnnotationConfigApplicationContext ctx =
+    //     new AnnotationConfigApplicationContext("io.github.jeanhwea.app02");
+
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+    ctx.registerBean(BeanLifeBeanPostProcessor.class);
+    ctx.registerBean(BeanLife.class);
+    ctx.refresh();
+
     // BeanLife beanLife = ctx.getBean("beanLife", BeanLife.class);
     System.out.println("====================  END  ====================");
   }
