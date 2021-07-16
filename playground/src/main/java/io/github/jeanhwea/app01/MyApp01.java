7219ab33fb91ee256730c0b599d07b91a0f4353f
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-16 08:55:30 +0800

    polish

--- playground/src/main/java/io/github/jeanhwea/app01/MyApp01.java
+++ playground/src/main/java/io/github/jeanhwea/app01/MyApp01.java
@@ -1,19 +1,53 @@
 package io.github.jeanhwea.app01;
 
 import io.github.jeanhwea.app01.bean.Author;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp01 {
 
-  public static void main(String[] args) {
-    ClassPathXmlApplicationContext ctx1 =
+  // XML 方式
+  public static void regBean01() {
+    ClassPathXmlApplicationContext ctx =
         new ClassPathXmlApplicationContext("META-INF/my-beans.xml");
-    Author author1 = ctx1.getBean("author", Author.class);
+    Author author1 = ctx.getBean("author", Author.class);
     System.out.println(author1.getName());
+  }
 
-    AnnotationConfigApplicationContext ctx2 = new AnnotationConfigApplicationContext(Author.class);
-    Author author2 = ctx2.getBean("author", Author.class);
+  // 注解方式
+  public static void regBean02() {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Author.class);
+    Author author2 = ctx.getBean("author", Author.class);
     System.out.println(author2.getName());
+  }
+
+  // 调用 registerBean 方法注册
+  public static void regBean03() {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+    ctx.registerBean(Author.class);
+    ctx.refresh();
+    Author author3 = ctx.getBean("author", Author.class);
+    System.out.println(author3.getName());
+  }
+
+  // 使用 BeanDefinition 注册
+  private static void regBean04() {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+    AbstractBeanDefinition beanDefinition =
+        BeanDefinitionBuilder.genericBeanDefinition().getBeanDefinition();
+    beanDefinition.setBeanClass(Author.class);
+    ctx.registerBeanDefinition("author", beanDefinition);
+    ctx.refresh();
+    Author author4 = ctx.getBean("author", Author.class);
+    System.out.println(author4.getName());
+  }
+
+  public static void main(String[] args) {
+    regBean01();
+    regBean02();
+    regBean03();
+    regBean04();
   }
 }
--- playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
+++ playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
@@ -2,20 +2,25 @@ package io.github.jeanhwea.app02;
 
 import io.github.jeanhwea.app02.lifecycle.BeanLife;
 import io.github.jeanhwea.app02.lifecycle.BeanLifeBeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp02 {
 
+  // 扫描包方式
+  public static void regBean01() {
+    AnnotationConfigApplicationContext ctx =
+        new AnnotationConfigApplicationContext("io.github.jeanhwea.app02");
+  }
+
+
+
   public static void main(String[] args) {
     System.out.println("==================== BEGIN ====================");
 
-    // AnnotationConfigApplicationContext ctx =
-    //     new AnnotationConfigApplicationContext("io.github.jeanhwea.app02");
-
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-    ctx.registerBean(BeanLifeBeanPostProcessor.class);
-    ctx.registerBean(BeanLife.class);
-    ctx.refresh();
+    // regBean01();
+    // regBean02();
 
     // BeanLife beanLife = ctx.getBean("beanLife", BeanLife.class);
     System.out.println("====================  END  ====================");
