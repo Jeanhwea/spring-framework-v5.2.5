755e64d206e10be08169312a316b6bbeaa44ca6c
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-16 08:56:54 +0800

    update

--- playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
+++ playground/src/main/java/io/github/jeanhwea/app02/MyApp02.java
@@ -1,28 +1,11 @@
 package io.github.jeanhwea.app02;
 
-import io.github.jeanhwea.app02.lifecycle.BeanLife;
-import io.github.jeanhwea.app02.lifecycle.BeanLifeBeanPostProcessor;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp02 {
 
-  // 扫描包方式
-  public static void regBean01() {
+  public static void main(String[] args) {
     AnnotationConfigApplicationContext ctx =
         new AnnotationConfigApplicationContext("io.github.jeanhwea.app02");
-  }
-
-
-
-  public static void main(String[] args) {
-    System.out.println("==================== BEGIN ====================");
-
-    // regBean01();
-    // regBean02();
-
-    // BeanLife beanLife = ctx.getBean("beanLife", BeanLife.class);
-    System.out.println("====================  END  ====================");
   }
 }
