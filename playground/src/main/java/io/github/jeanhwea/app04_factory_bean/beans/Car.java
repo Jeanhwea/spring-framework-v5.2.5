b2fd5da0df81a72bacd105d1a437b5100720f3a8
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-18 21:36:47 +0800

    add

--- playground/src/main/java/io/github/jeanhwea/app04_factory_bean/beans/Car.java
+++ playground/src/main/java/io/github/jeanhwea/app04_factory_bean/beans/Car.java
@@ -1,9 +1,12 @@
 package io.github.jeanhwea.app04_factory_bean.beans;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car {
+
+  @Value("Hong Qi")
   private String brand;
 
   public String getBrand() {
