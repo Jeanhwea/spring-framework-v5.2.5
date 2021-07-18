385b77d3ece000d23d88b0a1fd063c2d287d32ec
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-18 11:08:28 +0800

    add: 添加 Java 反射代码

--- playground/src/main/java/io/github/jeanhwea/app03_reflection/MyApp03.java
+++ playground/src/main/java/io/github/jeanhwea/app03_reflection/MyApp03.java
@@ -1,26 +1,41 @@
 package io.github.jeanhwea.app03_reflection;
 
+import io.github.jeanhwea.app03_reflection.bean.MyController;
+import io.github.jeanhwea.app03_reflection.bean.MyService;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class MyApp03 {
 
   public static Object prepareService(String message) throws Exception {
-    Class<?> clz = Class.forName("io.github.jeanhwea.app03_reflection.bean.MyService");
+    Class<?> serviceClass = Class.forName("io.github.jeanhwea.app03_reflection.bean.MyService");
 
-    Constructor<?> ctor = clz.getConstructor();
-    Object obj = ctor.newInstance();
+    Constructor<?> serviceConstructor = serviceClass.getConstructor();
+    Object serviceInstance = serviceConstructor.newInstance();
 
-    Field field = clz.getDeclaredField("message");
-    field.setAccessible(true);
-    field.set(obj, message);
+    Field messageField = serviceClass.getDeclaredField("message");
+    messageField.setAccessible(true);
+    messageField.set(serviceInstance, message);
 
-    // Method method = clz.getMethod("getMessage");
-    // String name = (String) method.invoke(obj);
-    return obj;
+    return serviceInstance;
+  }
+
+  public static Object prepareController(MyService myService) throws Exception {
+    Class<?> controllerClass =
+        Class.forName("io.github.jeanhwea.app03_reflection.bean.MyController");
+    Constructor<?> controllerConstructor = controllerClass.getConstructor(MyService.class);
+    Object controllerInstance = controllerConstructor.newInstance(myService);
+
+    Method echoMethod = controllerClass.getMethod("echo");
+    String response = (String) echoMethod.invoke(controllerInstance);
+    System.out.printf("Reflection call echo(): %s\n", response);
+    return controllerInstance;
   }
 
   public static void main(String[] args) throws Exception {
-    // System.out.println(name);
+    MyService myService = (MyService) prepareService("Hello from main");
+    MyController myController = (MyController) prepareController(myService);
+    System.out.printf("Directly call echo(): %s\n", myController.echo());
   }
 }
