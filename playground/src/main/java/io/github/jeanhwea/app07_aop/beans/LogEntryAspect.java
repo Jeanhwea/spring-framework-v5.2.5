bc7199457be74d8aab3cdf71380a23a9ad33c3c9
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-21 10:03:41 +0800

    add: 完成 Aspect 的调试

--- playground/src/main/java/io/github/jeanhwea/app07_aop/beans/LogEntryAspect.java
+++ playground/src/main/java/io/github/jeanhwea/app07_aop/beans/LogEntryAspect.java
@@ -1,8 +1,13 @@
 package io.github.jeanhwea.app07_aop.beans;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
+@EnableAspectJAutoProxy
 public class LogEntryAspect {
 
   @Pointcut("execution(* *.say(..))")
@@ -11,5 +16,23 @@ public class LogEntryAspect {
   @Before("say()")
   public void beforeSay() {
     System.out.println("beforeSay");
+  }
+
+  @After("say()")
+  public void afterSay() {
+    System.out.println("afterSay");
+  }
+
+  @Around("say()")
+  public Object aroundSay(ProceedingJoinPoint point) {
+    System.out.println("aroundSay/preActions");
+    Object obj = null;
+    try {
+      obj = point.proceed();
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+    System.out.println("aroundSay/postActions");
+    return obj;
   }
 }
