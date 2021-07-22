cb8b7f89f7e1196c06429c8fbf9029d7c837123e
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-22 22:20:54 +0800

    add

--- playground/src/main/java/io/github/jeanhwea/app08_bean_post_processor/MyApp08.java
+++ playground/src/main/java/io/github/jeanhwea/app08_bean_post_processor/MyApp08.java
@@ -8,5 +8,6 @@ public class MyApp08 {
     AnnotationConfigApplicationContext ctx =
         new AnnotationConfigApplicationContext(MyApp08.class.getPackage().getName());
     User user = ctx.getBean("user", User.class);
+    ctx.close();
   }
 }
